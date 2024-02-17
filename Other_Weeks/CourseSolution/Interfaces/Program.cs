2ba949a52dcd109551f736a1f3a51a9c065e4684
Nazlıcan Şahin <76625343+nazlicansahin@users.noreply.github.com>
2024-02-17 22:29:09 +0300

    [add] : RecapDemo desktop app [add] : Interfaces

---
+++ Other_Weeks/CourseSolution/Interfaces/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //InterfacesIntro();
+            Console.ReadLine();
+
+        }
+
+        private static void InterfacesIntro()
+        {
+            PersonManager personManager = new PersonManager();
+            Customer customer = new Customer { Id = 2, FirstName = "Nazli", LastName = "Sahin", Adress = "Ëskisehir" };
+            personManager.Add(customer);
+        }
+    }
+    interface IPerson
+    {
+        int Id { get; set; } 
+        string FirstName { get; set; }
+        string LastName { get; set; }
+
+    }
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Adress { get; set; }
+    }
+    class Student : IPerson
+    {
+        public int Id {get; set ; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+    }
+
+
+    class PersonManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName);
+        }
+        public void AddPerson(IPerson person)  //interfaci implemente eden butun classlar kullanabilir
+        {
+            Console.WriteLine(person.FirstName);
+
+        }
+    }
+}
