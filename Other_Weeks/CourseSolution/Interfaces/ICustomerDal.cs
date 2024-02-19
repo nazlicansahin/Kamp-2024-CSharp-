c034ce0e3151c8bf02b1ca038a0750e2b3450fab
Nazlıcan Şahin <76625343+nazlicansahin@users.noreply.github.com>
2024-02-20 00:40:19 +0300

    [add] : real world example for interface

---
+++ Other_Weeks/CourseSolution/Interfaces/ICustomerDal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal interface ICustomerDal  //Data access layer (dal)
+    {
+
+        void Add();
+        void Update();
+        void Delete();
+    }
+
+    class SqlServerCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Sql Added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Sql Delete");
+
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Sql Update");
+
+        }
+    }
+    class OracleServerCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Oracle Added");
+
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Oracle Delete");
+
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Oracle Update");
+
+        }
+    }
+    class CustomerManager
+    {
+
+    }
+
+
+}
