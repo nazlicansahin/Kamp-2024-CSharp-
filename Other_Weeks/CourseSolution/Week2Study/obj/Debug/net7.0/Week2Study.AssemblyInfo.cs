b72d037d89c74ce4e69bd29a8f7f79f7a005e803
Nazlıcan Şahin <76625343+nazlicansahin@users.noreply.github.com>
2024-02-22 22:36:30 +0300

    [add] : Language integrated query start

--- Other_Weeks/CourseSolution/Week2Study/obj/Debug/net7.0/Week2Study.AssemblyInfo.cs
+++ Other_Weeks/CourseSolution/Week2Study/obj/Debug/net7.0/Week2Study.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Week2Study")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+154266c7c0248e3ebd78f492d8f3cbe9e3f9e12d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43019c94e0c3b7eec8d45263615be63acdd7037f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Week2Study")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Week2Study")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
