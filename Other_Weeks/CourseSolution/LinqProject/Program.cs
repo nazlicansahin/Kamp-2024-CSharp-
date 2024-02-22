e5a98fba54f0be3294fd54fc41915b3cbca3ef19
Nazlıcan Şahin <76625343+nazlicansahin@users.noreply.github.com>
2024-02-22 23:20:40 +0300

    [add] : LINQ method

--- Other_Weeks/CourseSolution/LinqProject/Program.cs
+++ Other_Weeks/CourseSolution/LinqProject/Program.cs
@@ -10,6 +10,53 @@ namespace LinqProject
     {
         static void Main(string[] args)
         {
+            List<Category> categories = new List<Category>
+            {
+                new Category {CategoryId = 1, CategoryName= "Computer" },
+                new Category {CategoryId = 2, CategoryName= "PC" },
+                new Category {CategoryId = 3, CategoryName= "Tablet" },
+            };
+            List<Product> products = new List<Product>
+            {
+                new Product { ProductId = 1, CategoryId = 2, ProductName ="iphone ", QuantityPerUnit="256 gb", UnitInStock= 3, UnitPrice= 10000  },
+                new Product { ProductId = 1, CategoryId = 1, ProductName ="HP ", QuantityPerUnit="Victus", UnitInStock= 4, UnitPrice= 1000  },
+                new Product { ProductId = 1, CategoryId = 3, ProductName ="ipad ", QuantityPerUnit="256 gb", UnitInStock= 4, UnitPrice= 100000  },
+
+            };
+
+        foreach (var product in products)
+            {
+                if (product.UnitPrice > 1000 && product.UnitInStock >3)
+                { 
+                Console.WriteLine(product.ProductName);
+                }
+            }
+        //static List<Product> GetProducts()
+        //{
+
+        
+        //}
+        Console.ReadLine();
         }
+
     }
+
+        class Product
+        {
+            public int ProductId { get; set; }
+
+            public int CategoryId { get; set; }
+            public string ProductName { get; set; }
+            public string QuantityPerUnit { get; set; }
+            public decimal UnitPrice { get; set; }
+
+            public int UnitInStock { get; set; }
+        }
+
+        class Category
+        {
+            public int CategoryId { get; set; }
+            public string CategoryName { get; set; }
+
+        }
 }
