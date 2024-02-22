ebd00ffb09a62a9bd99b26f55e81ad8ff8f9d8b7
Nazlıcan Şahin <76625343+nazlicansahin@users.noreply.github.com>
2024-02-23 00:01:29 +0300

    [update] : LINQ Methods

--- Other_Weeks/CourseSolution/LinqProject/Program.cs
+++ Other_Weeks/CourseSolution/LinqProject/Program.cs
@@ -19,10 +19,20 @@ namespace LinqProject
             List<Product> products = new List<Product>
             {
                 new Product { ProductId = 1, CategoryId = 2, ProductName ="iphone ", QuantityPerUnit="256 gb", UnitInStock= 3, UnitPrice= 10000  },
-                new Product { ProductId = 1, CategoryId = 1, ProductName ="HP ", QuantityPerUnit="Victus", UnitInStock= 4, UnitPrice= 1000  },
+                new Product { ProductId = 1, CategoryId = 1, ProductName ="HP", QuantityPerUnit="Victus", UnitInStock= 4, UnitPrice= 1000  },
                 new Product { ProductId = 1, CategoryId = 3, ProductName ="ipad ", QuantityPerUnit="256 gb", UnitInStock= 4, UnitPrice= 100000  },
 
             };
+
+            Console.WriteLine( GetProductsWithLinq(products));
+
+            var wihtoutResult = GetProductsWithoutLinq(products);
+            foreach(var product in wihtoutResult)
+            {
+                Console.WriteLine( product.ProductName );
+            }
+
+            Console.WriteLine("---------Algoritmik-------------");
 
         foreach (var product in products)
             {
@@ -31,17 +41,46 @@ namespace LinqProject
                 Console.WriteLine(product.ProductName);
                 }
             }
-        //static List<Product> GetProducts()
-        //{
+            //static List<Product> GetProducts()
+            //{
 
-        
-        //}
+
+            //}
+            Console.WriteLine("---------------LINQ (Language integrated query) -----------------");
+
+        var result = products.Where(p => p.CategoryId == 1 && p.ProductName == "HP");
+        foreach(var product in result)
+            {
+            Console.WriteLine(product.ProductName);
+
+            }
         Console.ReadLine();
         }
+        static List<Product> GetProductsWithoutLinq(List<Product> products)
+        {
+            List<Product> filteredProducts = new List<Product>();
+            foreach (var product in products)
+            {
+                if (product.UnitPrice > 1000 && product.UnitInStock > 3)
+                {
+                    filteredProducts.Add(product);                
+                }
+            }
+            return filteredProducts;
+
+        }
+        
+        static List<Product> GetProductsWithLinq(List<Product> products) { 
+
+            return products.Where(p => p.CategoryId == 1 && p.ProductName == "HP").ToList();
+           
+        
+        }
+
 
     }
 
-        class Product
+    class Product
         {
             public int ProductId { get; set; }
 
